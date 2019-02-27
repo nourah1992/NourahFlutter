27cddcb517c41b8d466b19d0838d3972c88b401a
Nourah Abdulkareem <nourah1992@hotmail.com>
2019-02-27 19:42:03 +0300

    Add files via upload

--- main.dart
+++ main.dart
@@ -11,17 +11,16 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     //final wordPair = new WordPair.random();
     return new MaterialApp(
-      title: 'Welcome to Flutter :)',
-        home: new Scaffold(
-        appBar: new AppBar(
-        title: const Text('Welcome to Flutter'),
-    ),
-    body: new Center(
-    //child: new Text(wordPair.asPascalCase),
-      child: new RandomWords(),
-    ),
+      title: 'Startup Name Generator',
+        home: new RandomWords(),
 
-        ),
+    //),
+    //body: new Center(
+    //child: new Text(wordPair.asPascalCase),
+      //child: new RandomWords(),
+   // ),
+
+       // ),
     );
   }
 }
@@ -32,9 +31,48 @@ class RandomWords extends StatefulWidget {
 }
 
 class RandomWordsState extends State<RandomWords> {
+  final List<WordPair> _suggestions = <WordPair>[];
+  final TextStyle _biggerFont = const TextStyle(fontSize: 18.0);
   @override
   Widget build(BuildContext context) {
-    final WordPair wordPair = new WordPair.random();
-    return new Text(wordPair.asPascalCase);
+    //final WordPair wordPair = new WordPair.random();
+    //return new Text(wordPair.asPascalCase);
+
+    return new Scaffold (
+      appBar: new AppBar(
+        title: new Text('Startup Name Generator'),
+      ),
+      body: _buildSuggestions(),
+    );
+  }
+  Widget _buildSuggestions() {
+    return new ListView.builder(
+        padding: const EdgeInsets.all(16.0),
+        itemBuilder: (BuildContext _context, int i) {
+          // Add a one-pixel-high divider widget before each row
+          // in the ListView.
+          if (i.isOdd) {
+            return new Divider();
+          }
+          final int index = i ~/ 2;
+          // If you've reached the end of the available word
+          // pairings...
+          if (index >= _suggestions.length) {
+            // ...then generate 10 more and add them to the
+            // suggestions list.
+            _suggestions.addAll(generateWordPairs().take(10));
+          }
+          return _buildRow(_suggestions[index]);
+        }
+    );
+  }
+
+  Widget _buildRow(WordPair pair) {
+    return new ListTile(
+      title: new Text(
+        pair.asPascalCase,
+        style: _biggerFont,
+      ),
+    );
   }
 }
